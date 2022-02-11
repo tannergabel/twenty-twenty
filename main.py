1dfafbde4e67005c337493378dc988927f0b13b9
tgabel <tanner.gabel.2016@gmail.com>
2022-02-10 21:28:29 -0600

    Add frame interval constant.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 from cv2 import VideoCapture
 
 ESC_ASCII_CODE = 27
+FRAME_INTERVAL_IN_MS = 500
 
 camera = cv2.VideoCapture(0)
 
@@ -18,7 +19,7 @@ while True:
     else:
         print("Could not get image")
     
-    c = cv2.waitKey(1)
+    c = cv2.waitKey(FRAME_INTERVAL_IN_MS)
     if c == ESC_ASCII_CODE:
         break
 
